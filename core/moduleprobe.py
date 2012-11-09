964a5a09b897e945452bb74e5acc8e3056756784
Emilio <emilio.pinn@gmail.com>
2012-11-10 00:55:56 +0100

    Module system refactored for version v1

---
+++ core/moduleprobe.py
@@ -0,0 +1,95 @@
+
+
+from moduleexception import ModuleException, ProbeException, ProbeSucceed
+import types
+from core.savedargparse import SavedArgumentParser as ArgumentParser, Namespace
+
+class_name = 'Module'
+
+class ModuleProbe:
+    '''Generic class Module to inherit'''
+
+    # self.args: variables related to current execution
+    # self.stored_args: variables related to entire module life 
+
+    def __init__(self, modhandler):
+        
+
+        self.modhandler = modhandler
+
+        self.name = '.'.join(self.__module__.split('.')[-2:])
+        
+        self._init_module()
+
+    def run(self, arglist = []):
+        
+        self._output = ''
+        
+        try:
+            self._check_args(arglist)
+            self._prepare_probe()
+            self._probe()
+            self._verify_probe()
+        except ProbeException, e:
+            self.mprint('[!] Error: %s' % (e.error), 2, e.module) 
+        except ProbeSucceed, e:
+            pass
+        except ModuleException, e:
+            module = self.name
+            if e.module:
+                module = e.module
+            self.mprint('[!] Error: %s' % (e.error), 2, module) 
+            
+        return self._output
+
+    def mprint(self, str, msg_class = 3, module_name = None):
+        
+        if not self.modhandler.verbosity or msg_class <= self.modhandler.verbosity[-1]:
+            if not module_name:
+                module_name = self.name
+                
+            print '[%s] %s' % (module_name, str)
+            
+
+    def _init_module(self):
+        self.stored_args = {}
+    
+
+    def _check_args(self, args):
+
+        parsed_namespace, remaining_args = self.argparser.parse_known_stored_and_new_args(args=args, stored_args_dict = self.stored_args)
+        self.args = vars(parsed_namespace)
+        
+
+    def _prepare_probe(self):
+        pass
+    
+    def _verify_probe(self):
+        pass    
+
+    def _probe(self):
+        pass
+
+        
+    def save_args(self, args):
+
+        for argument in args:
+            if '=' in argument:
+                key, value = argument.split('=')
+                
+                # Reset value
+                if value == '':
+                    value = None
+                
+                self.stored_args[key] = value
+        
+                
+    def print_saved_args(self):
+    
+        saved_args_str = ''
+        for argument in [ action.dest for action in self.argparser._actions if action.dest != 'help' ]:
+            value = self.stored_args[argument] if argument in self.stored_args else ''
+            saved_args_str += '%s=\'%s\' ' % (argument, value)
+        self.mprint(saved_args_str)
+        
+    
---
+++ core/moduleprobeall.py
@@ -0,0 +1,66 @@
+from core.moduleprobe import ModuleProbe
+from core.moduleexception import ModuleException, ProbeException, ExecutionException, ProbeSucceed
+
+class ModuleProbeAll(ModuleProbe):
+
+
+    # self.args: variables related to current execution
+    # self.args['vector'] user input vector name
+    # self.stored_args: variables related to entire module life 
+    # self.stored_args['vector'] saved vector name
+    
+    # self.current_vector effective current vector object
+
+    def _init_module(self):
+        ModuleProbe._init_module(self)
+        self.current_vector = None
+        
+        
+    def __init_vector_variables(self, vector):
+        
+        self.args_formats = {}
+        self.current_vector = vector
+        
+    def _prepare_vector(self):
+        pass
+        
+    def _execute_vector(self):
+        self._output = self.current_vector.execute(self.modhandler, self.args_formats)
+    
+    def _verify_execution(self):
+        if self._output != '':
+            raise ProbeSucceed(self.name,'Command succeeded')
+     
+
+    def _probe(self):
+        
+        vectors = []
+        
+        if self.args['vector']:
+            selected_vector = self.vectors.get(self.args['vector'])
+            if selected_vector:
+                vectors = [ selected_vector ]
+        else:
+            vectors = self.vectors
+
+        try:
+            for vector in vectors:
+    
+                try:
+                    self.__init_vector_variables(vector)
+                    self._prepare_vector()
+                    self._execute_vector()
+                    self._verify_execution()
+                    
+                except ExecutionException:
+                    pass
+                
+        except ProbeSucceed:
+            # Execution succeed
+            pass
+        except ProbeException:
+            raise ModuleException(self.name,  "Probe failed")
+        
+        
+    
+

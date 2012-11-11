7557ebb0013304044a67dfbb6ab4475d952bddb6
Emilio <emilio.pinn@gmail.com>
2012-11-11 14:40:17 +0100

    Ported file.read to v1

---
+++ modules/file/read.py
@@ -0,0 +1,27 @@
+from modules.file.download import Download
+from tempfile import NamedTemporaryFile
+from core.savedargparse import SavedArgumentParser as ArgumentParser
+from core.moduleprobeall import ModuleProbe
+
+class Read(Download):
+    '''Read files from target filesystem
+:file.read <remote path> 
+    '''
+
+    argparser = ArgumentParser(usage=__doc__)
+    argparser.add_argument('rpath')
+    argparser.add_argument('-vector', choices = Download.vectors.get_names())
+
+    def _verify_execution(self):
+
+        file = NamedTemporaryFile()
+        file.close()
+
+        self.args['lpath'] = file.name
+        
+        return Download._verify_execution(self)
+    
+    def _output_result(self, stringify):
+        
+        return ModuleProbe._output_result(self, stringify)
+        

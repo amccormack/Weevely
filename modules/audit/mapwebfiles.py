438221f52fc3434220176de6de0bbc9060506721
Emilio <emilio.pinn@gmail.com>
2012-11-13 17:54:47 +0100

    Ported audit.mapwebfile to v1

---
+++ modules/audit/mapwebfiles.py
@@ -0,0 +1,55 @@
+from core.moduleprobe import ModuleProbe
+from core.moduleexception import ProbeException
+from core.vector import VectorList, Vector
+from core.savedargparse import SavedArgumentParser as ArgumentParser
+from external.crawler import Crawler
+from ast import literal_eval
+from core.prettytable import PrettyTable
+import os
+from modules.find.webdir import join_abs_paths
+
+class Mapwebfiles(ModuleProbe):
+    '''Enumerate webroot files properties '''
+
+
+    argparser = ArgumentParser(usage=__doc__)
+    argparser.add_argument('url', help='HTTP URL where start crawling (es. http://host/path/page.html)')
+    argparser.add_argument('baseurl', help='HTTP base url (es. http://host/path/)')
+    argparser.add_argument('rpath', help='Remote web root corresponding to crawled path (es. /var/www/path)', type=str)
+    argparser.add_argument('-deep', help='Crawl deepness', type=int, default=3)
+
+    support_vectors = VectorList([
+       Vector('file.enum', 'enum', ["asd", "-pathlist", "$pathlist"]),
+    ])
+
+    def _prepare_probe(self):
+    
+        url = self.args['url']    
+        baseurl = self.args['baseurl']
+        rpath = self.args['rpath']
+        
+        urls = []
+    
+        try:
+            crawler = Crawler(url, self.args['deep'], url, '')
+            crawler.crawl()
+        except Exception, e:
+            raise ProbeException(self.name, "Crawler exception: %s" % str(e))
+        else:
+            urls = set(crawler.visited_links.union(crawler.urls_seen))
+            
+            # If no url, or the only one is the specified one
+            
+            if not urls or (urls and len(urls) == 1 and list(urls)[0] == url):
+                raise ProbeException(self.name, "No sub URLs crawled. Check URL." )
+        
+        
+            self.args['paths'] = []
+            for path in urls:
+                self.args['paths'].append('/' + join_abs_paths([rpath, path[len(baseurl):]]))
+                
+
+
+    def _probe(self):
+
+        self._result = self.support_vectors.get('enum').execute(self.modhandler, {'pathlist' : str(self.args['paths']) })

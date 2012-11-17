9cd266fe939fe197f946983049459260cf3e8c32
Emilio Pinna <emilio.pinn@gmail.com>
2012-11-17 12:45:52 +0100

    Testsuite: fixed massive mem consumption loading conf file at every SimpleTest.setUp()

--- testsuite/testunits.py
+++ testsuite/testunits.py
@@ -8,24 +8,31 @@ from ConfigParser import ConfigParser
 import unittest, shlex
 import modules.shell.php
 from string import Template
-import pexpect
+from commands import getstatusoutput
 
 
 confpath = 'conf.ini'
 
+configparser = ConfigParser()
+configparser.read(confpath)
+conf = configparser._sections['global']
+
 class SimpleTestCase(unittest.TestCase):
     
     def setUp(self):
-        
-        config = ConfigParser()
-        config.read(confpath)
-        self.conf = config._sections['global']
-
         self._setenv()        
-        self.term = core.terminal.Terminal (ModHandler(self.conf['url'], self.conf['pwd']))
+        self.term = core.terminal.Terminal (ModHandler(conf['url'], conf['pwd']))
+
+
+    def tearDown(self):
+        self._unsetenv()
 
     def _setenv(self):
         pass
+    
+    def _unsetenv(self):
+        pass
+
 
     def __run_quietly(self, command):
         stdout = sys.stdout
@@ -42,13 +49,49 @@ class SimpleTestCase(unittest.TestCase):
     def _warn(self, command):
         self.__run_quietly(command)
         return self.term._last_warns
+    
 
-class ShellsTestCase(SimpleTestCase):
+    def _env_mkdir(self, relpath):
+        
+        abspath = os.path.join(conf['env_base_writable_web_dir'], relpath)
+        cmd = Template(conf['env_mkdir_command']).safe_substitute(path=abspath)
+        status, output = getstatusoutput(cmd)
+        print '[env_mkdir] %s' % cmd
+        if status != 0:
+            print output
+        
+    def _env_newfile(self, relpath, content = '1'):
+        abspath = os.path.join(conf['env_base_writable_web_dir'], relpath)
+        
+        cmd = Template(conf['env_mk_new_file_with_content']).safe_substitute(path=abspath, content=content)
+        status, output = getstatusoutput(cmd)
+        print '[env_newfile] %s' % cmd
+        if status != 0:
+            print output
 
-#    def _setenv(self):
-#        cmd = Template(self.conf['shell_copy_file']).safe_substitute(frompath=frompath, topath=topath)
-#        child = pexpect.spawn(cmd)
-#        child.interact()
+    def _env_chmod(self, relpath, mode='644'):
+        abspath = os.path.join(conf['env_base_writable_web_dir'], relpath)
+        
+        cmd = Template(conf['env_chmod_command']).safe_substitute(path=abspath, mode=mode)
+        print '[env_chmod] %s' % cmd
+        if status != 0:
+            print output
+
+    def _env_rm(self, relpath):
+        abspath = os.path.join(conf['env_base_writable_web_dir'], relpath)
+        
+        if conf['env_base_writable_web_dir'].count('/') != 3:
+            print 'Please check %s, not removing' % conf['env_base_writable_web_dir']
+            return
+        
+        cmd = Template(conf['env_rm_command']).safe_substitute(path=abspath)
+        status, output = getstatusoutput(cmd)
+        print '\n[env_rm] %s' % cmd
+        if status != 0:
+            print output
+
+class Shells(SimpleTestCase):
+
 
     def test_php(self):
         
@@ -75,7 +118,22 @@ class ShellsTestCase(SimpleTestCase):
         self.assertEqual(self._outp(':shell.sh \'(echo "VISIBLE" >&2)\' -stderr'), 'VISIBLE')
         self.assertEqual(self._outp(':shell.sh \'(echo "INVISIBLE" >&2)\''), '')
         
+
+class ShellsFSBrowse(SimpleTestCase):
+
+    def _setenv(self):
         
+        self.basedir = 'env_base_writable_web_dir'
+        self.newdirs = ['w1', 'w2', 'w3', 'w4']
+        self._env_mkdir(os.path.join(*self.newdirs))
+
+    def _unsetenv(self):
+        self._env_rm(self.newdirs[0])        
+        
+    def test_ls(self):
+        
+        self.assertEqual(self._outp(':shell.sh echo $((1+1))'), '2')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

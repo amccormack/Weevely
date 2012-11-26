7a2996fc9c3a57f0332990d2a4973f2b8f2941c9
Emilio Pinna <emilio.pinn@gmail.com>
2012-11-26 22:35:25 +0100

    Ported net.ifaces to v1

---
+++ modules/net/ifaces.py
@@ -0,0 +1,52 @@
+
+from core.moduleprobeall import ModuleProbe
+from core.moduleexception import ModuleException, ProbeException
+from core.vector import VectorList, Vector
+from core.savedargparse import SavedArgumentParser as ArgumentParser
+from external.ipaddr import IPNetwork
+import re
+
+WARN_NO_OUTPUT = 'No execution output'
+WARN_NO_IFACES = 'No interfaces address found'
+
+class Ifaces(ModuleProbe):
+    '''Print interface addresses'''
+    
+    support_vectors = VectorList([
+      Vector('file.enum', 'enum', ["asd", "-pathlist", "$pathlist"]),
+      Vector('shell.sh', "ifconfig" , "$ifconfig_path")
+    ])
+    
+    
+    argparser = ArgumentParser(usage=__doc__)
+    
+    
+    def _probe(self):
+        
+        self._result = {}
+        
+        enum_pathlist = str([ x + 'ifconfig' for x in ['/sbin/', '/bin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/', '/usr/local/sbin/'] ])
+
+        ifconfig_pathlist = self.support_vectors.get('enum').execute(self.modhandler, {'pathlist' : enum_pathlist })
+        
+        for path in ifconfig_pathlist:
+            if ifconfig_pathlist[path] != ['','','','']:
+                result = self.support_vectors.get('ifconfig').execute(self.modhandler, {'ifconfig_path' : path })
+                
+                if result:
+                    ifaces = re.findall(r'^(\S+).*?inet addr:(\S+).*?Mask:(\S+)', result, re.S | re.M)
+
+                    if ifaces:
+                        
+                        for iface in ifaces:
+                            ipnet = IPNetwork('%s/%s' % (iface[1], iface[2]))
+                            self._result[iface[0]] = ipnet
+                else:
+                    raise ProbeException(self.name, '\'%s\' %s' % (path, WARN_NO_OUTPUT))      
+                
+                
+    def _verify_probe(self):
+        if not self._result:
+            raise ProbeException(self.name, WARN_NO_IFACES)    
+                
+                     

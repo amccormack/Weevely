9cc1e1a8090e91896c23122842949eb73ec68695
Emilio <emilio.pinn@gmail.com>
2012-11-26 15:56:18 +0100

    Testsuite: add bruteforce.sql

--- testsuite/test_brutesql.py
+++ testsuite/test_brutesql.py
@@ -16,20 +16,20 @@ class BruteSQL(SimpleTestCase):
     def test_brutesql(self):
 
         expected_match = [ conf['bruteforce_sql_user'], conf['bruteforce_sql_pwd'] ]
-
-        self.assertEqual(self._res(':bruteforce.sql %s -wordlist "%s"' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()))), expected_match)
+        dbms = conf['bruteforce_sql_dbms']
+        self.assertEqual(self._res(':bruteforce.sql %s -wordlist "%s" -dbms %s' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()), dbms)), expected_match)
         
         temp_path = NamedTemporaryFile(); 
         temp_path.write('\n'.join(self._generate_wordlist()))
         temp_path.flush() 
         
-        self.assertEqual(self._res(':bruteforce.sql %s -wordfile "%s"' % (conf['bruteforce_sql_user'], temp_path.name)), expected_match)
-        self.assertRegexpMatches(self._warn(':bruteforce.sql %s -wordfile "%sunexistant"' % (conf['bruteforce_sql_user'], temp_path.name)), modules.bruteforce.sql.WARN_NO_SUCH_FILE)
+        self.assertEqual(self._res(':bruteforce.sql %s -wordfile "%s" -dbms %s' % (conf['bruteforce_sql_user'], temp_path.name, dbms)), expected_match)
+        self.assertRegexpMatches(self._warn(':bruteforce.sql %s -wordfile "%sunexistant" -dbms %s' % (conf['bruteforce_sql_user'], temp_path.name, dbms)), modules.bruteforce.sql.WARN_NO_SUCH_FILE)
         self.assertRegexpMatches(self._warn(':bruteforce.sql %s' % (conf['bruteforce_sql_user'])), modules.bruteforce.sql.WARN_NO_WORDLIST)
         
-        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 1 -wordlist "%s"' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()))), expected_match)
-        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 100000 -wordlist "%s"' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()))), expected_match)
-        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 0 -wordlist "%s"' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()))), expected_match)
+        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 1 -wordlist "%s" -dbms %s' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()), dbms)), expected_match)
+        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 100000 -wordlist "%s" -dbms %s' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()), dbms)), expected_match)
+        self.assertEqual(self._res(':bruteforce.sql %s -chunksize 0 -wordlist "%s" -dbms %s' % (conf['bruteforce_sql_user'], str(self._generate_wordlist()),dbms)), expected_match)
         
         # TODO: add -startline tests, wrong user/hostname, other dbms check
         

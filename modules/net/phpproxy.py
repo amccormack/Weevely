30aedca770df6b7636c31a50147f858f72fd364b
Emilio <emilio.pinn@gmail.com>
2012-11-29 15:22:00 +0100

    Refactored net.phpproxy to sue file.upload2web

--- modules/net/phpproxy.py
+++ modules/net/phpproxy.py
@@ -1,107 +1,35 @@
 from string import ascii_lowercase
-from core.moduleprobeall import ModuleProbe
+from modules.file.upload2web import Upload2web
 from core.moduleexception import ModuleException, ProbeException
 from core.vector import VectorList, Vector
 from core.savedargparse import SavedArgumentParser as ArgumentParser
+from argparse import SUPPRESS
 import re, os, random
 
-WARN_NOT_FOUND = 'Path not found'
-WARN_WRITABLE_WEB_FOLDER_NOT_FOUND = "Writable web directory not found"
-WARN_UPLOAD_FAIL = 'Upload fail, check path and permission'
-WARN_FILE_EXT = 'File name does not have \'.php\' extension'
-WARN_WEBROOT_INFO = 'Error getting web enviroinment informations'
-WARN_NOT_WEBROOT_SUBFOLDER = "is not a webroot subdirectory"
+class Phpproxy(Upload2web):
+    '''Upload binary/ascii file to the web root, getting the corresponding url'''
 
 
-def join_abs_paths(paths,sep = '/'):
-    return sep.join([p.strip(sep) for p in paths])
-
-class Phpproxy(ModuleProbe):
-    '''Install PHP proxy to target. Needs php-curl package remotely installed.'''
-    
-    support_vectors = VectorList([
-      Vector('system.info', 'document_root', 'document_root' ),
-      Vector('shell.php', 'normalize', 'print(realpath("$path"));'),                      
-      Vector('find.webdir', 'finddir', ["$rpath"]),
-      Vector('file.upload', 'upload', ["asd", "$rpath", "-content", "$content"]),
-      Vector('system.info', 'document_root', ["document_root"])
-    ])
-    
-    
     argparser = ArgumentParser(usage=__doc__)
-    argparser.add_argument('-find', help='Search install path starting from path', metavar='STARTFOLDER', default ='.')
-    argparser.add_argument('-install', help='Install php as remote path', metavar='PATH')
+    argparser.add_argument('rpath', help='Optional, upload as rpath', nargs='?')
     
-    def _check_web_root_folder(self, folder):
-            
-        document_root = self.support_vectors.get('document_root').execute(self.modhandler)
-        start_path = self.support_vectors.get('normalize').execute(self.modhandler, { 'path' : folder })
-        
-        if not document_root:
-            raise ProbeException(self.name, WARN_WEBROOT_INFO)
-        elif not start_path:
-            raise ProbeException(self.name, WARN_NOT_FOUND)
-        elif not start_path.startswith(document_root):
-            raise ProbeException(self.name, '\'%s\' %s' % (start_path, WARN_NOT_WEBROOT_SUBFOLDER) )        
-                
-    
-    def _prepare_probe(self):
-        
-        proxy_path = os.path.join(self.modhandler.path_modules, 'net', 'external', 'phpproxy.php')
-    
-        try:
-            f = open(proxy_path)
-        except IOError, e:
-            raise ProbeException(self.name,  str(e))
-        
-        self.args['content'] = f.read()
-    
-        if self.args['install']:
-            
-            # Check if normalized self.args['install_path'] is in document_root
-            # Extract path
-            install_folder = '/'.join(self.args['install'].split('/')[:-1])
-            self._check_web_root_folder(install_folder)
+    argparser.add_argument('-startpath', help='Upload in first writable subdirectory', metavar='STARTPATH', default='.')
+    argparser.add_argument('-chunksize', type=int, default=1024)
+    argparser.add_argument('-vector', choices = Upload2web.vectors.get_names(), help=SUPPRESS)
+    argparser.add_argument('-force', action='store_true')
 
-            # Check extension
-            if not self.args['install'].endswith('.php'):
-                raise ProbeException(self.name, WARN_FILE_EXT)
-            
-            self.args['install_path'] = self.args['install']
-            self.args['install_url'] = ''    
-        
-        else:
-            
-            result = self.support_vectors.get('finddir').execute(self.modhandler,{'rpath' : self.args['find']})
-            self._check_web_root_folder(self.args['find'])
-        
-            if not result:
-                raise ProbeException(self.name, "%s, specify exact path using -install" % WARN_WRITABLE_WEB_FOLDER_NOT_FOUND)
-            else:
-                filename = ''.join(random.choice(ascii_lowercase) for x in range(4)) + '.php'
-                self.args['install_path'] = '/' + join_abs_paths([result[0], filename])
-                self.args['install_url'] = join_abs_paths([result[1], filename])
-
+    def _check_args(self, arglist):
+        Upload2web._check_args(self, arglist)
+        self.args['lpath'] = os.path.join(self.modhandler.path_modules, 'net', 'external', 'phpproxy.php')
+        self.args['content'] = None
     
-    def _probe(self):
-        
-        result = self.support_vectors.get('upload').execute(self.modhandler, {'rpath' : self.args['install_path'], 'content' : self.args['content']})
-        if result:
-            self._result = [self.args['install_path'], self.args['install_url']]
-        else:
-            raise ProbeException(self.name, '\'%s\' %s' % (self.args['install_path'], WARN_UPLOAD_FAIL))
-            
+    
     def _output_result(self):
-        if not self.args['install_url']:
-            filename = self.args['install_path'].split('/')[-1]
-            url = '/'.join(self.modhandler.url.split('/')[:3]) + '/where/is/installed/' + filename
-        else:
-            url = self.args['install_url'] 
-            
-        sess_filename = join_abs_paths(self.args['install_path'].split('/')[:-1] + ['sess_*'])
-            
+
+        sess_filename = os.path.join(*(self.args['rpath'].split('/')[:-1] + [ 'sess_*']))
+        
         self._output = """Php proxy installed, point your browser to %s?u=http://www.google.com .
-Delete '%s' and '%s' at session end.""" % ( url, self.args['install_path'], sess_filename )
+Delete '%s' and '%s' at session end.""" % ( self.args['url'], self.args['rpath'], sess_filename )
         
             
         

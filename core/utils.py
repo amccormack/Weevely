7df8c3e79806482144f98011bbef22bed5c0c93a
Emilio Pinna <emilio.pinn@gmail.com>
2012-12-01 23:27:10 +0100

    Add URL validator in core.utils

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,14 @@
-
+from re import compile, IGNORECASE
 from base64 import b64encode
+
+
+url_validator = compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', IGNORECASE)
 
 def join_abs_paths(paths,sep = '/'):
     return sep.join([p.strip(sep) for p in paths])
@@ -7,3 +16,4 @@ def join_abs_paths(paths,sep = '/'):
 
 def b64_chunks(l, n):
     return [b64encode(l[i:i+n]) for i in range(0, len(l), n)]
+

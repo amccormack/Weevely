8e93132cb37985166a35f58374f0ec7fd22fd7f8
Emilio <emilio.pinn@gmail.com>
2012-12-10 22:14:46 +0100

    Added php backdoor generators generate.php, generate.img, generate.htaccess

--- modules/generate/php.py
+++ modules/generate/php.py
@@ -8,22 +8,32 @@ from core.moduleprobe import ModuleProbe
 from core.moduleexception import ModuleException
 from core.backdoor import Backdoor
 
+
+WARN_WRITING_DATA = 'Writing data'
+
 class Php(ModuleProbe):
     """Generate obfuscated PHP backdoor"""
 
     def _set_args(self):
-        self.argparser.add_argument('lpath', help='Path of generated backdoor')
         self.argparser.add_argument('pass', help='Password')
+        self.argparser.add_argument('lpath', help='Path of generated backdoor', default= 'weevely.php', nargs='?')
+
+    def _prepare_probe(self):
+        self.args['encoded_backdoor'] = Backdoor(self.args['pass']).backdoor
 
     def _probe(self):
         
         try:
-            file( self.args['lpath'], 'wt' ).write( Backdoor(self.args['pass']).backdoor )
+            file( self.args['lpath'], 'wt' ).write( self.args['encoded_backdoor'] )
         except Exception, e:
-            raise ModuleException(self.name,str(e))
+            raise ModuleException(self.name, "%s %s" % (WARN_WRITING_DATA, str(e)))
         else:
             self.mprint("Backdoor file '%s' created with password '%s'" % (self.args['lpath'], self.args['pass']))
-            
+        
+        self._result =  self.args['lpath']
+        
+    def _output_result(self):
+        pass
             
                     
         

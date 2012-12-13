67da8a6228fe4c3f1dcaf9d0caf16a195b9451ce
Emilio <emilio.pinn@gmail.com>
2012-12-13 16:39:23 +0100

    Testsuite: moved net.mount to find.mount

--- testsuite/test_mount.py
+++ testsuite/test_mount.py
@@ -5,10 +5,9 @@ import os, sys
 from string import ascii_lowercase
 sys.path.append(os.path.abspath('..'))
 import modules
-import modules.net.mount
+import modules.file.mount
 import modules.file.upload2web
 import modules.file.upload
-
 
 class Mount(FolderFSTestCase):
 
@@ -23,36 +22,37 @@ class Mount(FolderFSTestCase):
         
         self._outp('cd %s' % self.basedir)
         
-        res = self._res(':net.mount')
+        res = self._res(':file.mount')
         self.assertTrue(res and res[0].startswith(env_writable_baseurl) and res[0].endswith('.php') and res[1].startswith('/tmp/tmp') and res[2] == '.' )
         
-        res = self._res(':net.mount -remote-mount /tmp/')
+        res = self._res(':file.mount -remote-mount /tmp/')
         self.assertTrue(res and res[0].startswith(env_writable_baseurl) and res[0].endswith('.php') and res[1].startswith('/tmp/tmp') and res[2] == '/tmp/')
 
-        res = self._res(':net.mount -local-mount %s' % temp_dir)
+        res = self._res(':file.mount -local-mount %s' % temp_dir)
         self.assertTrue(res and res[0].startswith(env_writable_baseurl) and res[0].endswith('.php') and res[1] == temp_dir and res[2] == '.')
 
-        self.assertTrue(self._res(':net.mount -umount-all'))
+        self.assertTrue(self._res(':file.mount -umount-all'))
 
-        res = self._res(':net.mount -local-mount %s -rpath %s' % (temp_dir, temp_filename))
+        res = self._res(':file.mount -local-mount %s -rpath %s' % (temp_dir, temp_filename))
         self.assertTrue(res and res[0] == os.path.join(env_writable_baseurl, temp_filename) and res[1] == temp_dir and res[2] == '.')
         
-        res = self._res(':net.mount -rpath %s -force' % (temp_filename))
+        res = self._res(':file.mount -rpath %s -force' % (temp_filename))
         self.assertTrue(res and res[0] == os.path.join(env_writable_baseurl, temp_filename) and res[1].startswith('/tmp/tmp') and res[2] == '.')
         
-        res = self._res(':net.mount -startpath %s' % (self.dirs[0]))
+        res = self._res(':file.mount -startpath %s' % (self.dirs[0]))
         self.assertTrue(res and res[0].startswith(os.path.join(env_writable_baseurl,self.dirs[0])) and res[0].endswith('.php') and res[1].startswith('/tmp/tmp') and res[2] == '.')
 
-        res = self._res(':net.mount -just-mount %s ' % os.path.join(env_writable_baseurl, temp_filename))
+        res = self._res(':file.mount -just-mount %s ' % os.path.join(env_writable_baseurl, temp_filename))
         self.assertTrue(res and res[0] == os.path.join(env_writable_baseurl, temp_filename) and res[1].startswith('/tmp/tmp') and res[2] == '.')
         
-        res = self._res(':net.mount -just-install')
+        res = self._res(':file.mount -just-install')
         self.assertTrue(res and res[0].startswith(env_writable_baseurl) and res[0].endswith('.php') and res[1] == None and res[2] == '.' )
 
 
-        self.assertTrue(self._res(':net.mount -umount-all'))
-        self.assertRegexpMatches(self._warn(':net.mount -umount-all'), modules.net.mount.WARN_MOUNT_NOT_FOUND)
+        self.assertTrue(self._res(':file.mount -umount-all'))
+        self.assertRegexpMatches(self._warn(':file.mount -umount-all'), modules.file.mount.WARN_MOUNT_NOT_FOUND)
 
+        
     def test_mount_errors(self):
         
         temp_filename = randstr(4) + '.php'
@@ -63,18 +63,25 @@ class Mount(FolderFSTestCase):
         
         self._outp('cd %s' % self.basedir)
 
-        self.assertRegexpMatches(self._warn(':net.mount -remote-mount /nonexistant/'),modules.net.mount.WARN_HTTPFS_OUTP)
+        self.assertRegexpMatches(self._warn(':file.mount -remote-mount /nonexistant/'),modules.file.mount.WARN_HTTPFS_OUTP)
 
-        self.assertRegexpMatches(self._warn(':net.mount -local-mount /nonexistant/'),modules.net.mount.WARN_HTTPFS_OUTP)
+        self.assertRegexpMatches(self._warn(':file.mount -local-mount /nonexistant/'),modules.file.mount.WARN_HTTPFS_OUTP)
 
 
-        self.assertRegexpMatches(self._warn(':net.mount -rpath /notinwebroot'), modules.file.upload2web.WARN_NOT_WEBROOT_SUBFOLDER)
-        self.assertRegexpMatches(self._warn(':net.mount -rpath ./unexistant/path'), modules.file.upload2web.WARN_NOT_FOUND)
-        res = self._res(':net.mount -just-install -rpath %s' % (temp_filename))
-        self.assertRegexpMatches(self._warn(':net.mount -just-install -rpath %s' % (temp_filename)), modules.file.upload.WARN_FILE_EXISTS)
+        self.assertRegexpMatches(self._warn(':file.mount -rpath /notinwebroot'), modules.file.upload2web.WARN_NOT_WEBROOT_SUBFOLDER)
+        self.assertRegexpMatches(self._warn(':file.mount -rpath ./unexistant/path'), modules.file.upload2web.WARN_NOT_FOUND)
+        res = self._res(':file.mount -just-install -rpath %s' % (temp_filename))
+        self.assertRegexpMatches(self._warn(':file.mount -just-install -rpath %s' % (temp_filename)), modules.file.upload.WARN_FILE_EXISTS)
 
-        self.assertRegexpMatches(self._warn(':net.mount -startpath /notinwebroot'), modules.file.upload2web.WARN_NOT_FOUND)
-        self.assertRegexpMatches(self._warn(':net.mount -startpath ./unexistant/path'), modules.file.upload2web.WARN_NOT_FOUND)
+        self.assertRegexpMatches(self._warn(':file.mount -startpath /notinwebroot'), modules.file.upload2web.WARN_NOT_FOUND)
+        self.assertRegexpMatches(self._warn(':file.mount -startpath ./unexistant/path'), modules.file.upload2web.WARN_NOT_FOUND)
 
-        #self.assertRegexpMatches(self._warn(':net.mount -just-mount nonexistanthost'),modules.net.mount.WARN_HTTPFS_OUTP)
-        self.assertRegexpMatches(self._warn(':net.mount -just-mount %s/nonexistant' % env_writable_baseurl),modules.net.mount.WARN_HTTPFS_OUTP)
+        self.assertRegexpMatches(self._warn(':file.mount -just-mount localhost:9909'),modules.file.mount.WARN_HTTPFS_OUTP)
+        self.assertRegexpMatches(self._warn(':file.mount -just-mount %s/nonexistant' % env_writable_baseurl),modules.file.mount.WARN_HTTPFS_OUTP)
+
+    
+#        pathenv_splitted_orig = os.environ['PATH'].split(':')
+#        pathenv_splitted_orig.remove('/usr/local/bin')
+#        os.environ['PATH'] = ':'.join(pathenv_splitted_orig)
+#        
+#        self.assertRegexpMatches(self._warn(':file.mount -just-install'), modules.file.mount.WARN_ERR_RUN_HTTPFS)

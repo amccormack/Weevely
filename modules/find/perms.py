b0601b6c7997aafb3d3fcf4fba485e43ba89e1e6
Emilio <emilio.pinn@gmail.com>
2012-12-17 19:54:33 +0100

    Add -no-recursion in find.perms

--- modules/find/perms.py
+++ modules/find/perms.py
@@ -10,19 +10,19 @@ class Perms(ModuleProbeAll):
 
 
     def _set_vectors(self):
-        self.vectors.add_vector('php_recursive', 'shell.php', """$fdir='$rpath';$ftype='$type';$fattr='$attr';$fqty='$first';
-swp($fdir, $fdir,$ftype,$fattr,$fqty); 
+        self.vectors.add_vector('php_recursive', 'shell.php', """$fdir='$rpath';$ftype='$type';$fattr='$attr';$fqty='$first';$recurs=$recursion;
+swp($fdir, $fdir,$ftype,$fattr,$fqty,$recurs); 
 function ckprint($df,$t,$a) { if(cktp($df,$t)&&@ckattr($df,$a)) { print($df."\\n"); return True;}   }
 function ckattr($df, $a) { $w=strstr($a,"w");$r=strstr($a,"r");$x=strstr($a,"x"); return ($a=='')||(!$w||is_writable($df))&&(!$r||is_readable($df))&&(!$x||is_executable($df)); }
 function cktp($df, $t) { return ($t==''||($t=='f'&&@is_file($df))||($t=='d'&&@is_dir($df))); }
-function swp($fdir, $d, $t, $a, $q){ 
+function swp($fdir, $d, $t, $a, $q,$r){ 
 if($d==$fdir && ckprint($d,$t,$a) && ($q!="")) return; 
 $h=@opendir($d); while ($f = @readdir($h)) {
 $df=join('/', array(trim($d, '/'), trim($f, '/')));
 if(($f!='.')&&($f!='..')&&ckprint($df,$t,$a) && ($q!="")) return;
-if(($f!='.')&&($f!='..')&&cktp($df,'d')){@swp($fdir, $df, $t, $a, $q);}
+if(($f!='.')&&($f!='..')&&cktp($df,'d')&&$r){@swp($fdir, $df, $t, $a, $q,$r);}
 } closedir($h); }""")
-        self.vectors.add_vector("find" , 'shell.sh', "find $rpath $type $attr $first 2>/dev/null")
+        self.vectors.add_vector("find" , 'shell.sh', "find $rpath $recursion $type $attr $first 2>/dev/null")
     
     def _set_args(self):
         self.argparser.add_argument('rpath', help='Remote starting path', default ='.', nargs='?')
@@ -32,6 +32,8 @@ if(($f!='.')&&($f!='..')&&cktp($df,'d')){@swp($fdir, $df, $t, $a, $q);}
         self.argparser.add_argument('-readable', help='Matches redable files', action='store_true')
         self.argparser.add_argument('-executable', help='Matches executable files', action='store_true')
         self.argparser.add_argument('-vector', choices = self.vectors.keys())
+        self.argparser.add_argument('-no-recursion', help='Do not descend into subfolders', action='store_true', default=False)
+
 
     def _prepare_vector(self):
         
@@ -52,6 +54,9 @@ if(($f!='.')&&($f!='..')&&cktp($df,'d')){@swp($fdir, $df, $t, $a, $q);}
             self.args_formats['attr'] = '-writable' if self.args['writable'] else ''
             self.args_formats['attr'] += ' -readable' if self.args['readable'] else ''
             self.args_formats['attr'] += ' -executable' if self.args['executable'] else ''
+            
+            # Set recursion
+            self.args_formats['recursion'] =  ' -maxdepth 1 ' if self.args['no_recursion'] else ''
 
         else:
             # Vector.name = php_find
@@ -65,6 +70,10 @@ if(($f!='.')&&($f!='..')&&cktp($df,'d')){@swp($fdir, $df, $t, $a, $q);}
             self.args_formats['attr'] = 'w' if self.args['writable'] else ''
             self.args_formats['attr'] += 'r' if self.args['readable'] else ''
             self.args_formats['attr'] += 'x' if self.args['executable'] else ''
+            
+            # Set recursion
+            self.args_formats['recursion'] = not self.args['no_recursion']
+            
             
     def _output_result(self):
         

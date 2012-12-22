150151881619b880fd3ca05585c214179267d503
Emilio <emilio.pinn@gmail.com>
2012-12-22 10:51:01 +0100

    Testsuite: minor help fixes

--- testsuite/test_help.py
+++ testsuite/test_help.py
@@ -1,6 +1,9 @@
 from baseclasses import SimpleTestCase
 from tempfile import NamedTemporaryFile
-import os
+import os, sys
+sys.path.append(os.path.abspath('..'))
+import modules.shell.sh
+import modules.shell.php
 
 
 class FSEnum(SimpleTestCase):
@@ -11,13 +14,15 @@ class FSEnum(SimpleTestCase):
         self.assertNotRegexpMatches(help_output, '\n'*4)       
         
         help_shell_output = self._warn(":help shell" )
-        self.assertRegexpMatches(help_shell_output, '\[shell\.sh\] System shell[\s\S]+usage:[\s\S]+\[shell\.php\] PHP shell[\s\S]+usage:[\s\S]+')
+        sh_descr = modules.shell.sh.Sh.__doc__
+        php_descr = modules.shell.php.Php.__doc__
+        self.assertRegexpMatches(help_shell_output, '\[shell\.sh\] %s[\s\S]+usage:[\s\S]+\[shell\.php\] %s[\s\S]+usage:[\s\S]+' % (sh_descr, php_descr))
         self.assertNotRegexpMatches(help_shell_output, '\n'*4)       
         
         help_nonexistant_output = self._warn(":help nonexistant" )
         self.assertRegexpMatches(help_nonexistant_output, '')
         
         help_shell_sh_output = self._warn(":help shell.sh" )
-        self.assertRegexpMatches(help_shell_sh_output, 'usage:[\s\S]+System shell[\s\S]+positional arguments:[\s\S]+optional arguments:[\s\S]+stored arguments:[\s\S]+')
+        self.assertRegexpMatches(help_shell_sh_output, 'usage:[\s\S]+%s[\s\S]+positional arguments:[\s\S]+optional arguments:[\s\S]+stored arguments:[\s\S]+' % (sh_descr))
         self.assertNotRegexpMatches(help_shell_sh_output, '\n'*3)       
         

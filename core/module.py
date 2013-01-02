dd684d2d314df3dd7d6f78454c87c8ca66e04865
Emilio <emilio.pinn@gmail.com>
2013-01-02 22:40:53 +0100

    Add first documentation about module developing in of Module object

--- core/module.py
+++ core/module.py
@@ -6,215 +6,132 @@ from types import ListType, StringTypes, DictType
 from core.prettytable import PrettyTable
 from core.vector import VectorsDict
 from os import linesep
+from core.modulebase import ModuleBase
 
-
-class Module:
-    '''Generic class Module to inherit'''
-
-    # self.args: variables related to current execution
-    # self.stored_args: variables related to entire module life 
-
-    def __init__(self, modhandler):
+class Module(ModuleBase):
+    '''Generic Module class to inherit.
+    
+    To create new module, define MyModule class into python file "modules/group/mymodule.py". 
+    Class name needs to be equal to the module file name with first letter capitalized.
+    
+    Following methods are called only at first module execution by Module.__init__()
         
-
-        self.modhandler = modhandler
-
-        self.name = '.'.join(self.__module__.split('.')[-2:])
-
-        self._init_vectors()
-        self._init_args()
+        . Module._set_args() To declare argparse arguments [Optional]
+        . Module._set_vectors() To declare vector to use during run  [Optional]
+        . Module._init_module() For anything needed at module construction [Optional]
+    
+    Following methods are called at every run by Module.run() 
+    
+        . Module._prepare() Prepare vector and enviroinment for the probe [Optional]
+        . Module._probe() Effective vector run. [Mandatory]
+        . Module._verify() Verify if probe works [Optional]
+    
+    A basic Module declare at least arguments parameters in Module._set_args(), vectors 
+    declaration in Module._set_vectors(), and internal probe logic in Module._probe(). Read usage
+    details written in methods documentations. 
+    
+    Example of a basic module that download files from web into target, situated in "modules/net/webdownload.py":
+    
+        from core.module import Module
+        from core.moduleexception import ProbeException
+    
+        class Webdownload(Module):
         
-        self._set_vectors()
-        self._set_args()
+            def _set_args(self):
+                
+                # Declare accepted module parameters. Passed parameters are stored in self.args dictionary.
+                
+                self.argparser.add_argument('rpath', help='Remote path')
+                self.argparser.add_argument('lpath', help='Local path where save file')
+                
+            def _set_vectors(self):
+                
+                # Declare vectors to execute. First one contains code to call wget using 'shell.sh' system 
+                # shell interpreter module, second one calls 'file.check' existing module to verify
+                # downloaded file. Template fields like '$rpath' are replaced at vector execution.
+                
+                self.support_vectors.add_vector(name='wget', interpreter='shell.sh', payloads = [ 'wget', '$rpath', '-O', '$lpath' ])
+                self.support_vectors.add_vector(name='check_download', interpreter='file.check', payloads = [ '$rpath', 'exists' ])
+                
+            def _probe(self):
+            
+                # Start download calling 'wget' vector, formatting it with passed options in self.args dictionary.
+            
+                self.support_vectors.get('wget').execute(self.args)
+            
+            def _verify(self):
+            
+                # Verify downloaded file. Save vector return value in self._result and eventually raise 
+                # ProbeException to stop module execution and print error message.
+     
+                self._result = self.support_vectors.get('check_download').execute({ 'rpath' : self.args['rpath'] })
+                if self._result == False:
+                    raise ProbeException(self.name, 'Downloaded file not found')
+       
+       
+       
         
-        self.__init_module_variables()
-        self._init_module()
-        
-    def _init_vectors(self):
-        self.vectors = VectorsDict(self.modhandler)
-        self.support_vectors = VectorsDict(self.modhandler)
+    '''
 
-    def _init_args(self):
-        self.argparser = ArgumentParser(prog=':%s' % self.name, description = self.__doc__)
-        
         
     def _set_vectors(self):
+        """Inherit this method to add vectors in self.vectors and self.support_vectors lists, easily
+        callable in _probe() function. This method is called by module constructor. 
+        Example of vector declaration:
+        
+        > self.support_vectors.add_vector(name='vector_name', interpreter='module_name', payloads = [ 'module_param1', '$module_param2', .. ])
+        
+        Template fields like '$rpath' are replaced at vector execution.
+        
+        """
+        
         pass
     
     def _set_args(self):
+        """Inherit this method to set self.argparser arguments. Set new arguments following
+        official python argparse documentation like. This method is called by module constructor.
+        Arguments passed at module runs are stored in Module.args dictionary.
+        """
+        
         pass
         
-
-    def run(self, arglist = []):
-        
-        self._result = ''
-        self._output = ''
-
-        try:
-            self._check_args(arglist)
-            self._prepare()
-            self._probe()
-            self._verify()
-        except ProbeException, e:
-            self.mprint('[!] Error: %s' % (e.error), 2, e.module) 
-        except ProbeSucceed, e:
-            self._stringify_result()
-        except InitException, e:
-            raise
-        except ModuleException, e:
-            module = self.name
-            if e.module:
-                module = e.module
-            self.mprint('[!] Error: %s' % (e.error), 2, module) 
-        else:
-            self._stringify_result()
-            
-        
-        return self._result, self._output
-
-    def mprint(self, msg, msg_class = 3, module_name = None):
-        
-        if not self.modhandler.verbosity or msg_class <= self.modhandler.verbosity[-1]:
-            if module_name == None:
-                module_str = '[%s] ' % self.name
-            elif module_name == '':
-                module_str = ''
-            else:
-                module_str = '[%s] ' % module_name
-                
-            print module_str + str(msg)
-        
-            self.modhandler._last_warns += msg + linesep
-            
-
     def _init_module(self):
-        pass
+        """Inherit this method to set eventual additional variables. Called by module constructor.
+        """
     
-    def __init_module_variables(self):
-        self.stored_args = {}
-    
-    def _check_args(self, args):
-
-        parsed_namespace, remaining_args = self.argparser.parse_known_stored_and_new_args(args=args, stored_args_dict = self.stored_args)
-        self.args = vars(parsed_namespace)
-        
-
     def _prepare(self):
+        """Inherit this method to prepare vectors and enviroinment for the probe, using declared
+        vectors. 
+        
+        This method is called at every module run. Throws ModuleException, ProbeException.
+        """
+        
         pass
-    
-    def _verify(self):
-        pass    
 
     def _probe(self):
+        """Inherit this method to execute main tasks. Vector declared before are used to call
+        other modules and execute shell and php statements. This method is mandatory to execute tasks. 
+        
+        Example of vector selection and execution:
+        
+        > self.support_vectors.get('vector_name').execute({ '$module_param2' : self.args['arg2']})
+        
+        Vector is selected with VectorList.get(name=''), and launched by Vector.execute(templated_params={}), that
+        replace template variables and run it.
+        
+        Results that moudule execution returns should be stored in self._result. 
+        This method is called at every module run. Throws ModuleException, ProbeException, ProbeSucceed. 
+        
+        """
         pass
 
-    def _stringify_result(self):
-        
-        
-        # Empty outputs. False is probably a good output value 
-        if self._result != False and not self._result:
-            self._output = ''
-        # List outputs.
-        elif isinstance(self._result, ListType):
-            
-            if len(self._result) > 0:
-                
-                columns_num = 1
-                if isinstance(self._result[0], ListType):
-                    columns_num = len(self._result[0])
-                
-                table = PrettyTable(['']*(columns_num))
-                table.align = 'l'
-                table.header = False
-                
-                for row in self._result:
-                    if isinstance(row, ListType):
-                        table.add_row(row)
-                    else:
-                        table.add_row([ row ])
-            
-                self._output = table.get_string()
-                
-        # Dict outputs are display as tables
-        elif isinstance(self._result, DictType) and self._result:
-
-            # Populate the rows
-            randomitem = next(self._result.itervalues())
-            if isinstance(randomitem, ListType):
-                table = PrettyTable(['']*(len(randomitem)+1))
-                table.align = 'l'
-                table.header = False
-                
-                for field in self._result:
-                    table.add_row([field] + self._result[field])
-                
-            else:
-                table = PrettyTable(['']*2)
-                table.align = 'l'
-                table.header = False
-                
-                for field in self._result:
-                    table.add_row([field, str(self._result[field])])
-                
-
-                
-            self._output = table.get_string()
-        # Else, try to stringify
-        else:
-            self._output = str(self._result)
-        
-        
-    def store_args(self, args):
-
-        for argument in args:
-            if '=' in argument:
-                key, value = argument.split('=')
-                
-                # Reset value
-                if value == '':
-                    value = None
-                
-                self.stored_args[key] = value
-        
-    def format_help(self, help = True, stored_args=True,  name = True, descr=True, usage=True, padding = 0):
-        
-        help_output = ''
-
-        if help:
-            help_output += '%s\n' % self.argparser.format_help()
-        else:
-            
-            if name:
-                help_output += '[%s]' % self.name
-                
-            if descr:
-                if name: help_output += ' '
-                help_output += '%s\n' %self.argparser.description
-            
-            if usage:
-                help_output += '%s\n' % self.argparser.format_usage() 
     
-        stored_args_help = self.format_stored_args()
-        if stored_args and stored_args_help:
-            help_output += 'stored arguments: %s\n' % stored_args_help.replace('\n', '\n' + ' '*(18))
-            
-        help_output = ' '*padding + help_output.replace('\n', '\n' + ' '*(padding)).rstrip(' ') 
-            
-        return help_output
+    def _verify(self):
+        """Inherit this method to prepare vectors and enviroinment for the probe, using declared
+        vectors.
         
-                
-    def format_stored_args(self):
-    
-        stored_args_str = ''
-        i = 1
-        for argument in [ action.dest for action in self.argparser._actions if action.dest != 'help' ]:
-            value = self.stored_args[argument] if (argument in self.stored_args and self.stored_args[argument] != None) else ''
-            stored_args_str += '%s=\'%s\' ' % (argument, value)
-            
-            if i%4 == 0:
-                stored_args_str += '\n'
-            i+=1
-            
-        return stored_args_str
-        
-    
+        Results to print and return after moudule execution should be stored in self._result.
+        It is called at every module run. Throws ModuleException, ProbeException, ProbeSucceed.         
+        """
+        pass    
+

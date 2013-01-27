3a8f7fc40dc324f2bbb136187f3ecb249174b150
Emilio Pinna <emilio.pinn@gmail.com>
2013-01-27 15:22:09 +0100

    Proxy :net.proxy now runs as detached process

--- modules/net/proxy.py
+++ modules/net/proxy.py
@@ -3,80 +3,15 @@ from modules.net.phpproxy import Phpproxy
 from core.moduleexception import ProbeSucceed, ProbeException
 from core.storedargparse import StoredArgumentParser as ArgumentParser
 from argparse import SUPPRESS
-import re, os
+from os import path
 from random import choice
 from core.http.request import agent
 
-import SocketServer
-import urllib
-from thread import start_new_thread
 from core.utils import url_validator
+from subprocess import Popen
+from sys import executable
 
 WARN_NOT_URL = 'Not a valid URL'
-
-class ProxyHandler(SocketServer.StreamRequestHandler):
-
-    def __init__(self, request, client_address, server):
-
-        self.proxies = {}
-        self.useragent = agent
-        self.phpproxy = server.rurl
-
-        try:
-            SocketServer.StreamRequestHandler.__init__(self, request, client_address,server)
-        except Exception:
-            pass
-
-
-    def handle(self):
-        req, body, cl, req_len, read_len = '', 0, 0, 0, 4096
-        try:
-            while 1:
-                if not body:
-                    line = self.rfile.readline(read_len)
-                    if line == '':
-                        # send it anyway..
-                        self.send_req(req)
-                        return
-                    #if line[0:17].lower() == 'proxy-connection:':
-                    #    req += "Connection: close\r\n"
-                    #    continue
-                    req += line
-                    if not cl:
-                        t = re.compile('^Content-Length: (\d+)', re.I).search(line)
-                        if t is not None:
-                            cl = int(t.group(1))
-                            continue
-                    if line == "\015\012" or line == "\012":
-                        if not cl:
-                            self.send_req(req)
-                            return
-                        else:
-                            body = 1
-                            read_len = cl
-                else:
-                    buf = self.rfile.read(read_len)
-                    req += buf
-                    req_len += len(buf)
-                    read_len = cl - req_len
-                    if req_len >= cl:
-                        self.send_req(req)
-                        return
-        except Exception:
-            return
-
-    def send_req(self, req):
-        #print req
-        if req == '':
-            return
-        ua = urllib.FancyURLopener(self.proxies)
-        ua.addheaders = [('User-Agent', self.useragent)]
-        r = ua.open(self.phpproxy, urllib.urlencode({'req': req}))
-        while 1:
-            c = r.read(2048)
-            if c == '': break
-            self.wfile.write(c)
-        self.wfile.close()
 
 
 class Proxy(Phpproxy):
@@ -97,16 +32,11 @@ class Proxy(Phpproxy):
         self.argparser.add_argument('-vector', choices = self.vectors.keys(), help=SUPPRESS)
 
 
-    def _run_proxy_server(self, rurl, lport, lhost):
-
-        SocketServer.TCPServer.allow_reuse_address = True
-        server = SocketServer.ThreadingTCPServer((lhost, lport), ProxyHandler)
-        server.rurl = rurl
-        server.serve_forever()
-
-
     def _get_proxy_path(self):
-        return os.path.join(self.modhandler.path_modules, 'net', 'external', 'proxy.php')
+        return path.join(self.modhandler.path_modules, 'net', 'external', 'proxy.php')
+
+    def _get_local_proxy_path(self):
+        return path.join(self.modhandler.path_modules, 'net', 'external', 'local_proxy.py')
 
     def _prepare(self):
         
@@ -127,8 +57,8 @@ class Proxy(Phpproxy):
                 pass
             
         if not self.args['just_install']:
-            start_new_thread(self._run_proxy_server, (self.args['url'], self.args['lport'], self.args['lhost']))
-
+            self.pid = Popen([executable, self._get_local_proxy_path(), self.args['lhost'], str(self.args['lport']), self.args['url'], agent]).pid
+            
     def _verify(self):
         if not self.args['just_run']:
             Phpproxy._verify(self)   
@@ -144,9 +74,11 @@ class Proxy(Phpproxy):
         if self.args['rpath']:
             rpath = '\'%s\' ' % self.args['rpath']
         
+        self._result.append(self.pid)
+        
         self._output = """Proxy daemon spawned, set \'http://%s:%i\' as HTTP proxy to start browsing anonymously through target.
 Run ":net.proxy -just-run '%s'" to respawn local proxy daemon without reinstalling remote agent.
-When not needed anymore, remove %sremote agent.""" % (self.args['lhost'], self.args['lport'], self.args['url'], rpath)
+When not needed anymore, remove remote file with ":file.rm %s" and run locally 'kill -9 %i' to stop proxy.""" % (self.args['lhost'], self.args['lport'], self.args['url'], rpath, self.pid)
 
         
         
